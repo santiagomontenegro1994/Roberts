b51cbff86815d8e4b00b8918115b3cd05b61c971
PcAmilcar <pachycat94@gmail.com>
2025-05-17 12:39:08 -0300

    acomodando planilla de caja turno

--- funciones/select_general.php
+++ funciones/select_general.php
@@ -1125,17 +1125,26 @@ function ObtenerInfoCaja($vConexion, $idCaja) {
 }
 
 function Listar_Cajas($Conexion) {
-    $sql = "SELECT * FROM caja ORDER BY Fecha DESC";
-    $resultado = $Conexion->query($sql);
-    $Listado = [];
-    while ($fila = $resultado->fetch_assoc()) {
-        $Listado[] = $fila;
-    }
-    return $Listado;
+$sql = "SELECT caja.*, turnos.denominacion 
+        FROM caja 
+        LEFT JOIN turnos ON caja.idTurno = turnos.idTurno 
+        ORDER BY caja.Fecha DESC";
+        
+$resultado = $Conexion->query($sql);
+$Listado = [];
+while ($fila = $resultado->fetch_assoc()) {
+    $Listado[] = $fila;
+}
+return $Listado;
 }
 
 function Listar_Cajas_Parametro($Conexion, $Criterio, $Parametro) {
-    $sql = "SELECT * FROM caja WHERE $Criterio LIKE ? ORDER BY Fecha DESC";
+    $sql = "SELECT caja.*, turnos.denominacion 
+            FROM caja 
+            LEFT JOIN turnos ON caja.idTurno = turnos.idTurno 
+            WHERE caja.$Criterio LIKE ? 
+            ORDER BY caja.Fecha DESC";
+    
     $stmt = $Conexion->prepare($sql);
     $Parametro = "%$Parametro%";
     $stmt->bind_param("s", $Parametro);
